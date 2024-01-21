8835a8c9bba9e51e08f8562bc96b8fd3ca16c935
aaronwai <aaron.lung.wai@gmail.com>
2024-01-21 21:41:41 +0800

    step 19 : server action basic

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,4 +1,26 @@
+import { db } from "@/lib/db";
 const OrganizationIdPage = () => {
-  return <div>organization page !</div>;
+  async function create(formData: FormData) {
+    "use server";
+    const title = formData.get("title") as string;
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  }
+  return (
+    <div>
+      <form action={create}>
+        <input
+          className='border-black border p-1'
+          id='title'
+          name='title'
+          placeholder='Enter a board title'
+          required
+        />
+      </form>
+    </div>
+  );
 };
 export default OrganizationIdPage;
