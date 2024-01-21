13dc428fe0d5b1d77137d1aad7562b002b9adefe
aaronwai <aaron.lung.wai@gmail.com>
2024-01-21 22:24:13 +0800

    step 20 : server action I

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,14 +1,7 @@
 import { db } from "@/lib/db";
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
 const OrganizationIdPage = () => {
-  async function create(formData: FormData) {
-    "use server";
-    const title = formData.get("title") as string;
-    await db.board.create({
-      data: {
-        title,
-      },
-    });
-  }
   return (
     <div>
       <form action={create}>
@@ -19,6 +12,7 @@ const OrganizationIdPage = () => {
           placeholder='Enter a board title'
           required
         />
+        <Button type='submit'>Submit</Button>
       </form>
     </div>
   );
