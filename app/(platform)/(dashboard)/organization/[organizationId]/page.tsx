ce8d6f7601652c60e0630dee0b9e3de13f904888
aaronwai <aaron.lung.wai@gmail.com>
2024-01-22 20:55:06 +0800

    step 29 : Board form II

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,13 @@
 import { Info } from "./_components/info";
+import { Separator } from "@/components/ui/separator";
+import { BoardList } from "./_components/board-list";
+
 const OrganizationIdPage = async () => {
   return (
     <div className='flex flex-col space-y-4'>
       <Info />
+      <Separator className='my-4' />
+      <BoardList />
     </div>
   );
 };
