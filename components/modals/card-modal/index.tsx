4d07f1d8967941b71021831dcadc00cfa4aea315
aaronwai <aaron.lung.wai@gmail.com>
2024-03-04 12:38:19 +0800

    Step 55 card API basic

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -1,14 +1,23 @@
 "use client";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { useCardModal } from "@/hooks/use-card-modal";
+import { fetcher } from "@/lib/fetcher";
+import { CardWithList } from "@/types";
+import { Header } from "@radix-ui/react-accordion";
+import { useQuery } from "@tanstack/react-query";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
   const isOpen = useCardModal((state) => state.isOpen);
   const onClose = useCardModal((state) => state.onClose);
+
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ["card", id],
+    queryFn: () => fetcher(`/api/cards/${id}`),
+  });
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent>I am a Modal</DialogContent>
+      <DialogContent>{cardData?.title}</DialogContent>
     </Dialog>
   );
 };
