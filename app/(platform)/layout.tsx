f02b852b49bdc12b5eb007da7cf2074c316e334b
aaronwai <aaron.lung.wai@gmail.com>
2024-03-04 11:51:18 +0800

    Step 54 card Modal hook

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,10 +1,12 @@
 import { Toaster } from "sonner";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
       <Toaster />
+      <ModalProvider />
       {children}
     </ClerkProvider>
   );
