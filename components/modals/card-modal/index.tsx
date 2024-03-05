e3512b5fe946cf4bff4fd4aa14989652ebdfc52b
aaronwai <aaron.lung.wai@gmail.com>
2024-03-05 08:30:19 +0800

    Step 59 card modal - action buttons

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -6,6 +6,7 @@ import { CardWithList } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import { Header } from "./header";
 import { Description } from "./description";
+import { Actions } from "./actions";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -30,6 +31,7 @@ export const CardModal = () => {
               )}
             </div>
           </div>
+          {!cardData ? <Actions.Skeleton /> : <Actions data={cardData} />}
         </div>
       </DialogContent>
     </Dialog>
